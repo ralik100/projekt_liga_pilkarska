bd763f0d6774ffe7b0f6acdad38ab9ce8fdbd4ba
SBagniewski <sbagniewski051@gmail.com>
2024-05-06 15:30:32 +0200

    2nd commit
    creating insert file with basic values

---
+++ insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO termin (godzina_rozpoczecia,godzina_zakonczenia,data_meczu)
+VALUES (18,21,'2005-08-12');
+
+INSERT INTO termin (godzina_rozpoczecia,godzina_zakonczenia,data_meczu)
+VALUES (15,18,'2005-08-12');
+
+INSERT INTO termin (godzina_rozpoczecia,godzina_zakonczenia,data_meczu)
+VALUES (18,21,'2015-12-18');
+
